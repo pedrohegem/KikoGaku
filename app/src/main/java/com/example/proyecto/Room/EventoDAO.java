c4c89ad73bd26a42d648ec8502051797c0c5cb80
luhuertas <luhuertas@alumnos.unex.es>
2022-10-29 13:02:38 +0200

    Modificacion de EventoDAO.java

--- app/src/main/java/com/example/proyecto/Room/EventoDAO.java
+++ app/src/main/java/com/example/proyecto/Room/EventoDAO.java
@@ -1,7 +1,10 @@
 package com.example.proyecto.Room;
 
+import java.util.List;
 import androidx.room.Dao;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Delete;
 
 import java.util.List;
 
@@ -9,4 +12,18 @@ import java.util.List;
 public interface EventoDAO {
     @Query("SELECT * FROM eventoEntity")
     List<EventoEntity> getAll();
+
+    @Insert
+    public void insertar(Evento e);
+
+    //todo cambiar codigo por el codigo del evento
+    @Query("SELECT * FROM eventoEntity WHERE codigo = (:codigo)")
+    public List<Evento> obtenerEvento(String codigo);
+
+
+    @Query("SELECT * FROM eventoEntity WHERE codigoMunicipio = (:codigoMunicipio)")
+    public List<Evento> obtenerPorMunicipio(String codigoMunicipio);
+
+    @Delete
+    public void borrar(Evento e);
 }
