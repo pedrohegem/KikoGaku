0d8256720ddeeff6240244be56782b18a03c1716
JorgeDC <67222893+JorgeDC01@users.noreply.github.com>
2022-10-29 17:49:35 +0200

    Commit Modelo Evento añadido

---
+++ app/src/main/java/com/example/proyecto/Room/Modelo/Evento.java
@@ -0,0 +1,169 @@
+package com.example.proyecto.Room.Modelo;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+
+import com.example.proyecto.Room.javadb.DateConverter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Evento {
+
+    public final static SimpleDateFormat FORMAT = new SimpleDateFormat(
+            "yyyy-MM-dd", Locale.US);
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo
+    @NonNull
+    private int ide;
+
+    @ColumnInfo
+    private String titulo;
+
+    @ColumnInfo
+    private String ubicacion;
+
+    @ColumnInfo
+    private String descripcion;
+
+    @ColumnInfo
+    @TypeConverters(DateConverter.class)
+    private Date fecha;
+
+    private int probPrecipitacion;
+
+    private String estadoCielo;
+
+
+    private int velocidadViento;
+
+    private int tempMax;
+    private int tempMin;
+
+    private int sensacionTerminaMax;
+    private int getSensacionTerminaMin;
+
+
+    public Evento(int ide, String titulo, String ubicacion, String descripcion, String fecha, int probPrecipitacion, String estadoCielo, int velocidadViento, int tempMax, int tempMin, int sensacionTerminaMax, int getSensacionTerminaMin) {
+        this.ide = ide;
+        this.titulo = titulo;
+        this.ubicacion = ubicacion;
+        this.descripcion = descripcion;
+        this.probPrecipitacion = probPrecipitacion;
+        this.estadoCielo = estadoCielo;
+        this.velocidadViento = velocidadViento;
+        this.tempMax = tempMax;
+        this.tempMin = tempMin;
+        this.sensacionTerminaMax = sensacionTerminaMax;
+        this.getSensacionTerminaMin = getSensacionTerminaMin;
+
+        try {
+            this.fecha = Evento.FORMAT.parse(fecha);
+        } catch (ParseException e) {
+            this.fecha = new Date();
+        }
+
+    }
+
+    public int getIde() {
+        return ide;
+    }
+
+    public void setIde(int ide) {
+        this.ide = ide;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getProbPrecipitacion() {
+        return probPrecipitacion;
+    }
+
+    public void setProbPrecipitacion(int probPrecipitacion) {
+        this.probPrecipitacion = probPrecipitacion;
+    }
+
+    public String getEstadoCielo() {
+        return estadoCielo;
+    }
+
+    public void setEstadoCielo(String estadoCielo) {
+        this.estadoCielo = estadoCielo;
+    }
+
+    public int getVelocidadViento() {
+        return velocidadViento;
+    }
+
+    public void setVelocidadViento(int velocidadViento) {
+        this.velocidadViento = velocidadViento;
+    }
+
+    public int getTempMax() {
+        return tempMax;
+    }
+
+    public void setTempMax(int tempMax) {
+        this.tempMax = tempMax;
+    }
+
+    public int getTempMin() {
+        return tempMin;
+    }
+
+    public void setTempMin(int tempMin) {
+        this.tempMin = tempMin;
+    }
+
+    public int getSensacionTerminaMax() {
+        return sensacionTerminaMax;
+    }
+
+    public void setSensacionTerminaMax(int sensacionTerminaMax) {
+        this.sensacionTerminaMax = sensacionTerminaMax;
+    }
+
+    public int getGetSensacionTerminaMin() {
+        return getSensacionTerminaMin;
+    }
+
+    public void setGetSensacionTerminaMin(int getSensacionTerminaMin) {
+        this.getSensacionTerminaMin = getSensacionTerminaMin;
+    }
+}
