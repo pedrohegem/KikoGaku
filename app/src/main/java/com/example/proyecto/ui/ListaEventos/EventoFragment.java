e540e6f87deb64112153671567ef372b6b361b82
Mijarro <mijarro7@gmail.com>
2022-11-10 00:21:01 +0100

    FUNCIONAAAAAAAAA

--- app/src/main/java/com/example/proyecto/ui/ListaEventos/EventoFragment.java
+++ app/src/main/java/com/example/proyecto/ui/ListaEventos/EventoFragment.java
@@ -3,6 +3,7 @@ package com.example.proyecto.ui.ListaEventos;
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -15,11 +16,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.proyecto.R;
 import com.example.proyecto.Room.AppDatabase;
 import com.example.proyecto.Room.Modelo.Evento;
 import com.example.proyecto.Room.Modelo.EventoMontana;
+import com.example.proyecto.databinding.EventoListaBinding;
 import com.example.proyecto.ui.ListaEventos.placeholder.PlaceholderItem;
 
 import java.time.Instant;
@@ -34,6 +38,7 @@ import java.util.List;
 public class EventoFragment extends Fragment {
     private static final String ARG_COLUMN_COUNT = "column-count";
 
+    private Context mContext;
     private int mColumnCount = 1;
     private String nombre;
     private String fecha;
@@ -73,10 +78,10 @@ public class EventoFragment extends Fragment {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    for (int i=0; i<5;i++){
+                    /*for (int i=0; i<5;i++){
                         AppDatabase.getInstance(getContext()).eventoDAO().insertEvent(new Evento("Evento",i,"Descripcion", Calendar.getInstance().getTime()));
                         AppDatabase.getInstance(getContext()).eventoMontanaDAO().insertMontana(new EventoMontana("Evento","Madrid","Descripcion", Calendar.getInstance().getTime()));
-                    }
+                    }*/
                     List<Evento> eventos = AppDatabase.getInstance(getContext()).eventoDAO().getAll();
                     List<EventoMontana> eventosMontana = AppDatabase.getInstance(getContext()).eventoMontanaDAO().getAll();
                     Log.i("Recoleccion", "eventos: "+eventos.size());
@@ -118,5 +123,80 @@ public class EventoFragment extends Fragment {
             recyclerView.setAdapter(adapter);
         }
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        Log.i("TAG", "onCreate: Pruebaaaaaaaaaaa1");
+        super.onAttach(context);
+        this.mContext = context;
+    }
+
+    public class ListaEventosAdapter extends RecyclerView.Adapter<ListaEventosAdapter.ViewHolder> {
+
+        private final List<PlaceholderItem> mValues;
+
+        public ListaEventosAdapter(List<PlaceholderItem> items) {
+            mValues = items;
+            Log.i("TAG", "onCreate: Pruebaaaaaaaaaaa2");
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            ViewHolder holder = new ViewHolder(EventoListaBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(mContext,Borrate.class);
+                    intent.putExtra("EventoId",holder.mItem.id);
+                    intent.putExtra("Evento?",holder.mItem.evento);
+                    intent.putExtra("Fecha",holder.mItem.fecha);
+                    intent.putExtra("nombre",holder.mItem.nombre);
+                    mContext.startActivity(intent);
+                }
+            });
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mNombreView.setText(mValues.get(position).nombre);
+            holder.mFechaView.setText(mValues.get(position).fecha);
+            if(mValues.get(position).evento){
+                holder.mIcono.setImageResource(R.drawable.ic_evento);
+            }else{
+                holder.mIcono.setImageResource(R.drawable.ic_evemontanas);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        /* Representa a la fila que se muestra */
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final TextView mIdView;
+            public final TextView mNombreView;
+            public final TextView mFechaView;
+            public final ImageView mIcono;
+            public PlaceholderItem mItem;
+
+            public ViewHolder(EventoListaBinding binding) {
+                super(binding.getRoot());
+                mIdView = binding.itemNumber;
+                mNombreView = binding.Nombre;
+                mFechaView = binding.Fecha;
+                mIcono = binding.iconoEvento;
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mNombreView.getText() + "'";
+            }
+        }
     }
 }
