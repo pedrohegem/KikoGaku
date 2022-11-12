ac1dc1b56c2600f516e0a80a80407533698eda7e
Jorge <jorgecastillounex01@gmail.com>
2022-11-12 21:04:40 +0100

    Commit 4

---
+++ app/src/main/java/com/example/proyecto/ui/Localizaciones/LocalizacionesActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyecto.ui.Localizaciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyecto.R;
+
+public class LocalizacionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_localizaciones);
+    }
+}
