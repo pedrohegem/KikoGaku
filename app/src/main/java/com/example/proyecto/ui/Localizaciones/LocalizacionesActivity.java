fd531682834be7edac90ae2753ef497c23b174a2
Jorge <jorgecastillounex01@gmail.com>
2022-11-12 21:19:42 +0100

    Commit 5

--- app/src/main/java/com/example/proyecto/ui/Localizaciones/LocalizacionesActivity.java
+++ app/src/main/java/com/example/proyecto/ui/Localizaciones/LocalizacionesActivity.java
@@ -1,16 +1,44 @@
 package com.example.proyecto.ui.Localizaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.proyecto.R;
+import com.example.proyecto.databinding.ActivityCrearEventoBinding;
 
 public class LocalizacionesActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityCrearEventoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_localizaciones);
+        binding = ActivityCrearEventoBinding.inflate(getLayoutInflater());
+
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Evento");
+
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_crear_evento);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_crear_evento);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
 }
