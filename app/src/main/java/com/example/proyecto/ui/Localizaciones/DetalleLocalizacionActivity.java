b3f742537b40a9a3c47ca16a9b9aa76d66d5c709
Jorge <jorgecastillounex01@gmail.com>
2022-11-13 01:24:21 +0100

    Filtrado Municipios
    Se ha realizado el filtrado de municipios mediante la creación de dos Actividades, una se encarga del listado mientras que la segunda se encarga del detalle.

---
+++ app/src/main/java/com/example/proyecto/ui/Localizaciones/DetalleLocalizacionActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyecto.ui.Localizaciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleLocalizacionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_localizacion);
+    }
+}
