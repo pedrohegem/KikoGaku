9dc2f49c0feff36b87dc9e68b815d42c42b70ed1
Jorge <jorgecastillounex01@gmail.com>
2022-11-28 14:08:43 +0100

    UC01 - Test funcional. SIN ACABAR
    Este commit se hace para poder trabajar desde el portatil en el lab

---
+++ app/src/androidTest/java/com/example/proyecto/CrearEventoMunicipioTest.java
@@ -0,0 +1,277 @@
+package com.example.proyecto;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.GrantPermissionRule;
+
+import com.example.proyecto.ui.Eventos.CrearEventoActivity;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CrearEventoMunicipioTest {
+
+    @Rule
+    public ActivityScenarioRule<CrearEventoActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(CrearEventoActivity.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION",
+                    "android.permission.ACCESS_COARSE_LOCATION");
+
+    @Test
+    public void crearEventoMunicipioTest() {
+ /*       ViewInteraction materialButton = onView(
+                allOf(withId(R.id.bRegistrarse), withText("Registrarse"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.username),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("jo"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("1234"), closeSoftKeyboard());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.bRegistrarse), withText("Registrarse"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.username),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("jo"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText4.perform(replaceText("1234"), closeSoftKeyboard());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.bIniciarSesion), withText("Iniciar Sesión"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton3.perform(click());
+*/
+/*        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab), withContentDescription("BotonAnadirNuevoEvento"),
+                        childAtPosition(
+                                allOf(withId(R.id.app_bar_main),
+                                        childAtPosition(
+                                                withId(R.id.drawer_layout),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+*/
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.CrearMunicipio), withText("Evento de municipio"),
+                        childAtPosition(
+                                allOf(withId(R.id.LayoutCrearEvento),
+                                        childAtPosition(
+                                                withId(R.id.nav_host_fragment_content_crear_evento),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.InputNombreEvento),
+                        childAtPosition(
+                                allOf(withId(R.id.LayoutModificar),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                0)));
+        appCompatEditText5.perform(scrollTo(), replaceText("Futbol "), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.inputLocalidad),
+                        childAtPosition(
+                                allOf(withId(R.id.LayoutModificar),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                1)));
+        appCompatEditText6.perform(scrollTo(), replaceText("Sevilla"), closeSoftKeyboard());
+/*
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.InputFechaEvento),
+                        childAtPosition(
+                                allOf(withId(R.id.LayoutModificar),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                3)));
+        appCompatEditText7.perform(scrollTo(), click());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(android.R.id.button1),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton5.perform(click());
+*/
+        onView(withId(R.id.InputFechaEvento)).perform(typeText("30/01/2023"), closeSoftKeyboard());
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.InputDescripcionEvento),
+                        childAtPosition(
+                                allOf(withId(R.id.LayoutModificar),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                2)));
+        appCompatEditText8.perform(scrollTo(), replaceText("Con amigos"), closeSoftKeyboard());
+
+        //pressBack();
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(R.id.BotonModificar), withText("Crear evento"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1)));
+        materialButton6.perform(scrollTo(), click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.EtiquetaDetalles), withText("Futbol "),
+                        withParent(allOf(withId(R.id.LayoutCrear),
+                                withParent(withId(R.id.LayoutDetallesEvento)))),
+                        isDisplayed()));
+        textView.check(matches(withText("Futbol ")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.DetallesLocalidad), withText("Sevilla"),
+                        withParent(allOf(withId(R.id.LayoutCrear),
+                                withParent(withId(R.id.LayoutDetallesEvento)))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Sevilla")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.DetallesFechaDeInicio), withText("29/11/2022"),
+                        withParent(allOf(withId(R.id.LayoutCrear),
+                                withParent(withId(R.id.LayoutDetallesEvento)))),
+                        isDisplayed()));
+        textView3.check(matches(withText("29/11/2022")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.DetallesDescripcion), withText("Con amigos"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class))),
+                        isDisplayed()));
+        textView4.check(matches(withText("Con amigos")));
+
+        ViewInteraction materialButton7 = onView(
+                allOf(withId(R.id.BotonEliminar), withText("Eliminar"),
+                        childAtPosition(
+                                allOf(withId(R.id.LayoutDetallesEvento),
+                                        childAtPosition(
+                                                withId(R.id.nav_host_fragment_content_borrate),
+                                                0)),
+                                3),
+                        isDisplayed()));
+        materialButton7.perform(click());
+
+        ViewInteraction materialButton8 = onView(
+                allOf(withId(android.R.id.button1), withText("Confirmar"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        materialButton8.perform(scrollTo(), click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
